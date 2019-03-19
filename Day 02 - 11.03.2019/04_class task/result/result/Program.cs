aa28344b1e34cf09b3a4a2a88a79f2a5ca54fc74
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-03-19 09:55:13 +0200

    up

---
+++ Day 02 - 11.03.2019/04_class task/result/result/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Queue<string>> queueManager = new Dictionary<string, Queue<string>>();
+
+            queueManager.Add("Bank",new Queue<string>());
+            queueManager.Add("Change", new Queue<string>());
+            queueManager.Add("Post", new Queue<string>());
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter your name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter your requiered service (Bank / Change / Post)");
+                string service = Console.ReadLine();
+
+                if (queueManager.ContainsKey(service))
+                    queueManager[service].Enqueue(name);
+            }
+            */
+
+            queueManager["Post"].Enqueue("A");
+            queueManager["Post"].Enqueue("B");
+            queueManager["Post"].Enqueue("C");
+
+            queueManager["Change"].Enqueue("D");
+
+            queueManager["Bank"].Enqueue("E");
+        
+
+            int emtyCnt = 0;
+            while (emtyCnt < queueManager.Count)
+            {
+                emtyCnt = 0;
+                foreach (KeyValuePair<string, Queue<string>> item in queueManager)
+                {
+                    if (item.Value.Count > 0)
+                        Console.WriteLine($"queue {item.Key} is waiting for {item.Value.Dequeue()}");
+                    else
+                        emtyCnt++;
+                }
+                Console.WriteLine("----------------------");
+
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Advanced-csharp-and-ASP-2019B-II-
+# Advanced-csharp-and-ASP-2019B-II
+
+```
+The C# guide provides many resources about the C# language. This site has many different audiences. Depending on your experience with programming, or with the C# language and .NET, you may wish to explore different sections of this guide.
+```

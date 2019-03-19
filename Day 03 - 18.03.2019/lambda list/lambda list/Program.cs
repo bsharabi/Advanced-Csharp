91fb5afb5ead7cdf8ab53b20f0aee00be510ab8f
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-03-19 11:36:53 +0200

    UP

---
+++ Day 03 - 18.03.2019/lambda list/lambda list/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lambda_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            int k = 3;
+            List<Student> myStudent = new List<Student>();
+            myStudent.AddRange(new Student[]{
+                new Student(100,18,"barak"),
+                new Student(85,23,"ofek"),
+                new Student(98,26,"ariel"),
+                new Student(56,31,"igal"),
+                new Student(75,24,"nana")
+            });
+
+            Console.WriteLine("---------All----------");
+            myStudent.ForEach(x => Console.WriteLine(x.GetInfo()));
+
+            Console.WriteLine("--------Age-----------");
+            myStudent.Sort((x, y) => x.Age.CompareTo(y.Age));
+            myStudent.ForEach(x => Console.WriteLine(x.GetInfo()));
+
+            Console.WriteLine("---------Greade----------");
+            myStudent.Sort((x, y) => x.Greade.CompareTo(y.Greade));
+            myStudent.ForEach(x => Console.WriteLine(x.GetInfo()));
+
+            Console.WriteLine("----------Name---------");
+            myStudent.Sort((x, y) => x.Name.CompareTo(y.Name));
+            myStudent.ForEach(x => Console.WriteLine(x.GetInfo()));
+
+            Console.WriteLine("----------Name---------");
+            myStudent.Sort((x, y) => x.Name.CompareTo(y.Name));
+            myStudent.ForEach(x => Console.WriteLine(x.GetInfo()));
+            
+
+
+        }
+    }
+}
---
+++ Day 03 - 18.03.2019/lambda list/lambda list/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lambda_list
+{
+    class Student
+    {
+        private int greade;
+
+        public int Greade
+        {
+            get { return greade; }
+            set {
+                if(value>=0 && value<=100)
+                     greade = value; }
+        }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 18 && value <= 120)
+                    age = value;
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string GetInfo()
+        {
+            return $"The name is :{name}\nAge is:{age}\nGread :{greade}\n";
+        }
+
+        public Student(int greade, int age, string name)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Greade = greade;
+        }
+
+        public Student()
+        {
+
+        }
+    }
+}

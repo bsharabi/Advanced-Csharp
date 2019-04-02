b1203a1db1730c7a83f8d9e6b5bbc97a917bc44f
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-04-02 13:03:03 +0300

    auto push

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import glob
 str2=""
- 
 def GetFolder1(path,space):
     global str2
     a = glob.glob(path)
@@ -17,6 +16,8 @@ def GetFolder1(path,space):
                         str2=str2+("## "+a[i][:-1]+"\n")
                         str2=str2+("***\n")
                 else:   
+                        if "bin" in str(a[i]) or "Properties" in str(a[i]) or "obj" in str(a[i]) or ".idea" in str(a[i]) or "out" in str(a[i]) or "src" in str(a[i]):
+                                return
                         str2=str2+(space+"["+str(a[i]).split("\\")[len(str(a[i]).split("\\"))-1]+"]"+"(https://github.com/bsharabi/Advanced-CSharp/tree/master/{})".format(str1)+"\n")
                 GetFolder1(str(a[i]+"\\*"),"  "+space)        
 
@@ -28,9 +29,12 @@ def RenameFolder():
 
 GetFolder1("*\\","- ")
 
+
+
 file = open("Readme.md","w") 
 file.write("# Design-Patterns\n\n") 
 file.write(str2)    
 file.close()
 #RenameFolder()
+
 
